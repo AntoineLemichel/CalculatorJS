eb73868b458cbe0af54e293f2a34cb3d94a521f2
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-09-03 11:45:31 +0200

    ADding calculator

--- js/main.js
+++ js/main.js
@@ -5,31 +5,30 @@ let equal = document.getElementById("equal");
 let operator = document.getElementsByClassName("operators");
 let clear = document.getElementById("clear");
 
-
 for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", function () {
-    getValue(this);
-  });
+    numbers[i].addEventListener("click", function () {
+        getValue(this);
+    });
 }
 
 for (let i = 0; i < operator.length; i++) {
-  operator[i].addEventListener("click", function () {
-    getValue(this);
-  });
+    operator[i].addEventListener("click", function () {
+        getValue(this);
+    });
 }
 
 clear.addEventListener("click", function () {
-  resultat = "";
-  display.innerHTML = "0";
+    resultat = "";
+    display.innerHTML = "0";
 });
 
 function getValue(element) {
-  resultat += element.innerHTML;
-  display.innerHTML = resultat;
+    resultat += element.innerHTML;
+    display.innerHTML = resultat;
 }
 
 equal.addEventListener("click", function () {
-  var resultTotal = eval(resultat);
-  eval(resultTotal);
-  display.innerHTML = resultTotal;
+    var resultTotal = eval(resultat);
+    eval(resultTotal);
+    display.innerHTML = resultTotal;
 });
